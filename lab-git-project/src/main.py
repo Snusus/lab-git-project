fc150d86c49c6ea2a693425deaa1c77702709480
Snusus <130651437+Snusus@users.noreply.github.com>
2025-01-26 15:05:19 +0300

    First commit for new feature

--- lab-git-project/src/main.py
+++ lab-git-project/src/main.py
@@ -9,3 +9,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+#stash change with no commit
--- lab-git-project/src/utils.py
+++ lab-git-project/src/utils.py
@@ -1,5 +1,4 @@
 # utils.py
-
 def add_numbers(a, b):
     """Добавляет два числа."""
     return a + b
